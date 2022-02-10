71c338586ad17167816328dcc180e513e3e0df1b
can101 <can896373@gmail.com>
2022-02-10 13:46:33 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("listening on *:" + process.env.PORT);
+});
